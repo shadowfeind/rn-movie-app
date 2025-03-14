de4196c77a6741062e19f061a63167321de0d6e2
shadowfeind <travestroy007@gmail.com>
2025-03-14 15:42:55 +0545

    batman and robin

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,17 @@ module.exports = {
     extend: {
       colors: {
         primary: "#030014",
+        secondary: "#151312",
+        light: {
+          100: "#d6c5ff",
+          200: "#a8b5db",
+          300: "#9ca4ab",
+        },
+        dark: {
+          100: "#221f3d",
+          200: "#0f0d23",
+        },
+        accent: "#ab8bff",
       },
     },
   },
